ec8c5975e6edd1578f54b4a8d4dd23be70cb87c8
Marcos Paulo da Silva <mpsacademico@gmail.com>
2016-06-05 20:11:53 -0300

    Inserção do rer e códigos de operação

--- index.php
+++ index.php
@@ -1,31 +1,47 @@
 <?php	
 $retorno = array();
+$op = 0;
+$st = 0;
 require_once("funcoes.php");
 switch($_SERVER['REQUEST_METHOD']){
-	case 'GET':
-		if(isset($_GET['categoria']) && !empty($_GET['categoria'])){
-			if(strcmp($_GET['categoria'],"lanche")==0){
-				if(isset($_GET['codigo']) && !empty($_GET['codigo'])){					
+	case 'GET':		
+		if(isset($_GET['categoria']) && !empty($_GET['categoria'])){			
+			if(strcmp($_GET['categoria'],"lanche")==0){				
+				if(isset($_GET['codigo']) && !empty($_GET['codigo'])){
+					$op = 1;
+					$st = 200;
 					le($_GET['codigo']);
 				}				
 				else{
+					$op = 2;
+					$op = "2";
+					$st = 200;
 					lc();
 				}
 			}
 			else{
-				$retorno = mer("XXX","Categoria indisponível ou inexistente");
+				$op = 3;
+				$st = 404;
+				$retorno = mer("404","Categoria indisponível ou inexistente");
 			}			
 		}else{
-			$retorno = array("estado"=>"XXX","erro"=>"Argumentos da requisição inválidos");
+			$op = 4;
+			$st = 404;
+			$retorno = array("estado"=>"404","erro"=>"Argumentos da requisição inválidos");
 		}
 		break;
 	case 'POST':	
 		break;
 	default:
-		$retorno = array("requisicao"=>array("estado"=>"404","erro"=>"Método HTTP não permitido"));
+		$st = 405;
+		$retorno = array("requisicao"=>array("estado"=>"405","erro"=>"Método HTTP não permitido"));
 		break;	
 }	
+	$rm = $_SERVER['REQUEST_METHOD'];
+	$rt = $_SERVER['REQUEST_TIME'];
+	$rea = $_SERVER['REMOTE_ADDR'];
+	rer($op, $st,$rm,$rt,$rea);
 //header('Content-Type: application/json');
-$retorno = json_encode($retorno,true);
+//$retorno = json_encode($retorno,true);
 //echo $retorno;
 ?>
